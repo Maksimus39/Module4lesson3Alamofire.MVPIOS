c90de009f8f67070255c15ce4f0782df57401389
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-05-19 20:15:15 +0300

    add layer

---
+++ Module4lesson3Alamofire.MVPIOS/Network/NetworkManager.swift
@@ -0,0 +1,43 @@
+import Foundation
+import Alamofire
+
+protocol NetworkManagerProtocol: AnyObject {
+    func request<T: Decodable>(completion: @escaping (Result<T, Error>) -> Void)
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    func request<T>(completion: @escaping (Result<T, any Error>) -> Void) where T : Decodable {
+        guard let url = URL(string: "https://stoic.tekloon.net/stoic-quote") else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        let params: Parameters? = nil
+        AF.request(url, parameters: params)
+            .validate(statusCode: 200..<300)
+            .response { res in
+                if let error = res.error {
+                    completion(.failure(error))
+                    return
+                }
+                guard let data = res.data else {
+                    completion(.failure(URLError(.cannotDecodeRawData)))
+                    return
+                }
+                do {
+                    let decoded = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+    }
+}
+
+struct StoicQuoteResponse: Decodable {
+    let data: StoicQuote
+}
+
+struct StoicQuote: Decodable {
+    let author: String
+    let quote: String
+}
--- Module4lesson3Alamofire.MVPIOS/ViewController.swift
+++ Module4lesson3Alamofire.MVPIOS/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Module4lesson3Alamofire.MVPIOS
-//
-//  Created by Максим Минаков on 03.05.2026.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         print("Hello IOS")
     }
 }
